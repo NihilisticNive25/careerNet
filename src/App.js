b9d610156de23aa2e4b4a1cbf6513eba88edc79d
NihilisticNive25 <nivedhasamy@gmail.com>
2019-02-10 23:08:36 +0530

    params update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import OpportunityDisplay from './Components/opportunityDisplay.js'
 const initialState = {
          opportunity : {},
        selectedBackgrounds : [],
-      selectedSkills : [],     
+      selectedSkills : [], 
+      OpportunityID : "" ,   
 }
 class App extends React.Component {
 
@@ -18,114 +19,10 @@ class App extends React.Component {
 	}
 
 componentWillMount = () => {
-
   document.title = "Job Search";
-
-  let getOpportunityByID = `https://api-staging.aiesec.org/v2/opportunities/6125?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c`
-      fetch(getOpportunityByID)
-      .then(res => res.json())
-      .then((result) => {         
-      this.setState({opportunity : result})
-       
-      let bacgroundObj = [];
-            result.backgrounds.forEach(x =>bacgroundObj.push({value : x.id, label : x.name}) )            
-            this.setState({backgrounds : bacgroundObj});
-
-      let skillsObj = [];
-      result.skills.forEach(x =>skillsObj.push({value : x.id, label : x.name}) )            
-      this.setState({skills : skillsObj});
-
-
-      })
-      .catch(err => console.log(err));
-
-  let getBackgrounds = `https://api-staging.aiesec.org/v2/lists/backgrounds?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c`
-      fetch(getBackgrounds)
-      .then(res => res.json())
-      .then((result) => {     
-        let bacgroundObj = []; 
-        result.forEach(x =>bacgroundObj.push({value : x.id, label : x.name}) )   
-        this.setState({backgroundOptions : bacgroundObj})
-        
-      })
-      .catch(err => console.log(err));
-
-
-  let getSkills = `https://api-staging.aiesec.org/v2/lists/skills?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c`
-      fetch(getSkills)
-      .then(res => res.json())
-      .then((result) => {   
-        let skillObj = [];
-        result.forEach(x =>skillObj.push({value : x.id, label : x.name}) )     
-        this.setState({skillOptions : skillObj})
-      })
-      .catch(err => console.log(err));
-
 }
 
-handleSelectChange = name => value => {
- 
-  let opportunity = Object.assign({}, this.state.opportunity);
-     let resetObj = [];
-        value.forEach(x =>resetObj.push({id : x.value, name : x.label, "option": "preferred", "level": 0}) ) 
 
-  opportunity[name] = {};
-  opportunity[name] = resetObj;
-  this.setState({opportunity});
-
-  this.state[name]= [];
-  this.state[name] = value;                       
-  this.setState({name : value});
-  
-};
-
-handleLocationChange = (e) => {
-
-let opportunity = Object.assign({}, this.state.opportunity); 
-     this.setState({opportunity : {...opportunity,"role_info":{ 
-       ...opportunity["role_info"],
-        "city":e.name
-    }}});      
-
-  };
-
-  handleDateChange = name => value  => {
-
-    let opportunity = Object.assign({}, this.state.opportunity);  
-    let dateformat = new Date(value);
-    opportunity[name] = dateformat.toISOString(); 
-    this.setState({opportunity});      
-
-  };
- 
-  handleChange = (event) => {
-    
-    const target = event.target;
-    const value =  target.value;
-    const name = target.name;
-
-    let opportunity = Object.assign({}, this.state.opportunity);         
-    opportunity[name] = value;   
-                         
-    this.setState({opportunity});
-
-  }
-
-
-  handleNestedObjChange = (event) => {
-
-    const target = event.target;
-    const value =  target.value;
-    const name = target.name;
-    const keys = name.split('.');
-
-    let opportunity = Object.assign({}, this.state.opportunity);         
-    opportunity[keys[0]][keys[1]] = value;                      
-    this.setState({opportunity});
- 
-  }
-
- 
 
   render() {
   	
@@ -135,23 +32,8 @@ let opportunity = Object.assign({}, this.state.opportunity);
       <Router  >
       <div>
        <Route path="/opportunity/:OpportunityID" component={OpportunityDisplay}></Route>
-       
-       <Route path='/editOpportunity/:OpportunityID' 
-        render={({props}) => <Opportunity {...props}
-         opportunity={opportunity} 
-         backgroundOptions = {backgroundOptions}
-         skillOptions = {skillOptions}
-         backgrounds = {backgrounds} 
-         skills = {skills} 
-         handleDateChange = {this.handleDateChange}
-         handleChange={this.handleChange}
-         handleSelectChange = {this.handleSelectChange}
-         handleNestedObjChange = {this.handleNestedObjChange}
-         handleLocationChange = {this.handleLocationChange}
-           />
-         }
-                 
-        
+        <Route path='/editOpportunity/:OpportunityID' 
+        component ={() => <Opportunity/> }        
         />
         </div>
       </Router>
--- src/Components/opportunity.js
+++ src/Components/opportunity.js
@@ -4,21 +4,132 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import Autocomplete from 'react-google-autocomplete';
 import { Redirect } from 'react-router-dom';
-import ReactDOM from "react-dom";;
-
+import ReactDOM from "react-dom";
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
  class opportunity extends React.Component {
  constructor(props){
   super(props);
   this.state = {
     redirect: false,
-    errors: [],
+    errors: [],  
+    opportunity:{},
+    backgrounds:[],
+    backgroundOptions:[],
+    skills:[],
+    skillOptions:[]    
   }
 }
+
+componentWillMount = () => {
+
+  let getOpportunityByID = `https://api-staging.aiesec.org/v2/opportunities/${this.props.match.params.OpportunityID}?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c`
+      fetch(getOpportunityByID)
+      .then(res => res.json())
+      .then((result) => {         
+      this.setState({opportunity : result})
+       
+      let bacgroundObj = [];
+            result.backgrounds.forEach(x =>bacgroundObj.push({value : x.id, label : x.name}) )            
+            this.setState({backgrounds : bacgroundObj});
+
+      let skillsObj = [];
+      result.skills.forEach(x =>skillsObj.push({value : x.id, label : x.name}) )            
+      this.setState({skills : skillsObj});
+
+
+      })
+      .catch(err => console.log(err));
+
+  let getBackgrounds = `https://api-staging.aiesec.org/v2/lists/backgrounds?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c`
+      fetch(getBackgrounds)
+      .then(res => res.json())
+      .then((result) => {     
+        let bacgroundObj = []; 
+        result.forEach(x =>bacgroundObj.push({value : x.id, label : x.name}) )   
+        this.setState({backgroundOptions : bacgroundObj})
+        
+      })
+      .catch(err => console.log(err));
+
+
+  let getSkills = `https://api-staging.aiesec.org/v2/lists/skills?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c`
+      fetch(getSkills)
+      .then(res => res.json())
+      .then((result) => {   
+        let skillObj = [];
+        result.forEach(x =>skillObj.push({value : x.id, label : x.name}) )     
+        this.setState({skillOptions : skillObj})
+      })
+      .catch(err => console.log(err));
+} 
+
+
+handleSelectChange = name => value => {
  
+  let opportunity = Object.assign({}, this.state.opportunity);
+     let resetObj = [];
+        value.forEach(x =>resetObj.push({id : x.value, name : x.label, "option": "preferred", "level": 0}) ) 
+
+  opportunity[name] = {};
+  opportunity[name] = resetObj;
+  this.setState({opportunity});
+
+  this.state[name]= [];
+  this.state[name] = value;                       
+  this.setState({name : value});
+  
+};
+
+handleLocationChange = (e) => {
+
+let opportunity = Object.assign({}, this.state.opportunity); 
+     this.setState({opportunity : {...opportunity,"role_info":{ 
+       ...opportunity["role_info"],
+        "city":e.name
+    }}});      
+
+  };
+
+  handleDateChange = name => value  => {
+
+    let opportunity = Object.assign({}, this.state.opportunity);  
+    let dateformat = new Date(value);
+    opportunity[name] = dateformat.toISOString(); 
+    this.setState({opportunity});      
+
+  };
+ 
+  handleChange = (event) => {
+    
+    const target = event.target;
+    const value =  target.value;
+    const name = target.name;
+
+    let opportunity = Object.assign({}, this.state.opportunity);         
+    opportunity[name] = value;   
+                         
+    this.setState({opportunity});
+
+  }
+
+
+  handleNestedObjChange = (event) => {
+
+    const target = event.target;
+    const value =  target.value;
+    const name = target.name;
+    const keys = name.split('.');
+
+    let opportunity = Object.assign({}, this.state.opportunity);         
+    opportunity[keys[0]][keys[1]] = value;                      
+    this.setState({opportunity});
+ 
+  }
+
     render(){
 
-const { opportunity,handleChange,handleDateChange,handleSelectChange, 
-    backgroundOptions, skillOptions ,backgrounds,skills, handleNestedObjChange, handleLocationChange} = this.props
+   const {opportunity,backgroundOptions, skillOptions ,backgrounds,skills } = this.state;
 
 let url = `/opportunity/${opportunity.id}`;
 
@@ -80,8 +191,6 @@ const updateOpportunity = (e) => {
   .then(res => res.json())
   .then(data => {
     this.setState({redirect : true});
-    
-        console.log(data);
   })
 
 }
@@ -121,7 +230,7 @@ const updateOpportunity = (e) => {
 	        <div className="w-90 w-40-m w-50-l tableColumnForm">
 	          <label  className="labelCSS">Title </label>
 	          <input  className="inputStyle title w-100" 
-	          onChange={handleChange.bind(this)}
+	          onChange={this.handleChange}
 	          name = "title"
 	          type="text" 
 	          defaultValue={opportunity.title}/>
@@ -134,7 +243,7 @@ const updateOpportunity = (e) => {
 	           <DatePicker
 	           className="datepickerStyle"
 	              selected={opportunity.earliest_start_date}
-	        	  onChange={handleDateChange('earliest_start_date')}
+	        	  onChange={this.handleDateChange('earliest_start_date')}
 	        	  defaultValue={null}
 	     		/>
 	        </div>
@@ -148,7 +257,7 @@ const updateOpportunity = (e) => {
 	           <DatePicker
 	           className="datepickerStyle applicationDate"
 	              selected={opportunity.applications_close_date}
-	        	  onChange={handleDateChange('applications_close_date')}
+	        	  onChange={this.handleDateChange('applications_close_date')}
 	        	  defaultValue={null}
 	     		/>
 	        </div>
@@ -160,7 +269,7 @@ const updateOpportunity = (e) => {
 	           <DatePicker
 	           className="datepickerStyle"
 	              selected={opportunity.latest_end_date}
-	        	  onChange={handleDateChange('latest_end_date')}
+	        	  onChange={this.handleDateChange('latest_end_date')}
 	        	  defaultValue={null}
 	     		/>
 	        </div>
@@ -171,7 +280,7 @@ const updateOpportunity = (e) => {
 	        <div className="w-90 w-40-m w-50-l tableColumnForm">
 	          <label  className="labelCSS">Duration<span className="normal black-60">(In weeks)</span></label>
 	          <input  className="inputStyle w-100" 
-	          onChange={handleChange.bind(this)}
+	          onChange={this.handleChange}
 	          name = "project_duration"
 	          type="text" 
 	          defaultValue={opportunity.project_duration}/>
@@ -181,7 +290,7 @@ const updateOpportunity = (e) => {
 	        
 	          <label  className="labelCSS">Positions</label>
 	          <input  className="inputStyle w-100" 
-	          onChange={handleChange.bind(this)}
+	          onChange={this.handleChange}
 	          name = "available_openings"
 	          type="text" defaultValue={opportunity.available_openings}/>
 	          
@@ -192,7 +301,7 @@ const updateOpportunity = (e) => {
 	        <div className="w-90 w-40-m w-50-l tableColumnForm">
 	          <label  className="labelCSS">Salary <span className="normal black-60">(In dollars)</span>  </label>
 	          <input  className="inputStyle w-100" 
-	          onChange={handleNestedObjChange.bind(this)}
+	          onChange={this.handleNestedObjChange}
 	          name = "specifics_info.salary"
 	          type="text" 
 	          value={opportunity.specifics_info ? opportunity.specifics_info.salary : ""}/>
@@ -206,7 +315,7 @@ const updateOpportunity = (e) => {
                    name="opportunity.role_info"
    				 types={['address']}
    				 defaultValue={opportunity.role_info ? opportunity.role_info.city : ""}
-                 onPlaceSelected={handleLocationChange.bind(this)}
+                 onPlaceSelected={this.handleLocationChange}
 />
 	        </div>
         </div>
@@ -222,7 +331,7 @@ const updateOpportunity = (e) => {
           classNamePrefix ="w-100"
           name  = "backgrounds"
           isSearchable 
-          onChange={handleSelectChange('backgrounds')}
+          onChange={this.handleSelectChange('backgrounds')}
           />
 	        </div>
 	        <div className="w-90 w-40-m w-50-l tableColumnForm" >
@@ -236,7 +345,7 @@ const updateOpportunity = (e) => {
           classNamePrefix ="w-100"
           name  = "skills"
           isSearchable 
-          onChange={handleSelectChange('skills')}
+          onChange={this.handleSelectChange('skills')}
           />
 	          
 	        </div>
@@ -245,14 +354,14 @@ const updateOpportunity = (e) => {
 		<div className="w-90 w-40-m w-50-l tableColumnForm" >
           <label className="labelCSS">Description <span className="normal black-60">(Max 180 characters)</span></label>
           <textarea value={opportunity.description} 
-          onChange={handleChange.bind(this)}
+          onChange={this.handleChange}
           name = "description"
           className="textareaStyle w-100 tableColumnForm " aria-describedby="comment-desc"></textarea>
  		 </div>
  		 <div className="w-90 w-40-m w-50-l tableColumnForm">
               <label  className="labelCSS">Selection Process</label>
               <input  className="inputStyle w-100" 
-              onChange={handleNestedObjChange.bind(this)}
+              onChange={this.handleNestedObjChange}
               name = "role_info.selection_process"
               type="text" 
               value={opportunity.role_info ? opportunity.role_info.selection_process : ""}/>
@@ -278,4 +387,4 @@ const updateOpportunity = (e) => {
 
 
 
-export default opportunity;
+export default withRouter(opportunity);
